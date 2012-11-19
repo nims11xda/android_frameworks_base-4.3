7199faa435b7a924a208ee4bc6a73d13dc175fe8
Stevespear426 <stephen.k.spear@gmail.com>
2012-11-18 22:34:58 -0500

    navbar

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3462,6 +3462,17 @@ public final class Settings {
         public static final String NOTIFICATIONS_BEHAVIOUR = "notifications_behaviour";
 
         /**
+         * Custom navring actions
+         *
+         * @hide
+         */
+        public static final String[] NAVIGATION_RING_TARGETS = new String[] {
+            "navigation_ring_targets_0",
+            "navigation_ring_targets_1",
+            "navigation_ring_targets_2",
+        };
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
